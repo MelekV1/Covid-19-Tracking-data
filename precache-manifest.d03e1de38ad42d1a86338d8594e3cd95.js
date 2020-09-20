3ae3a1e0f1cc1d22034adb919a613e5408bb0b63
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2020-09-20 15:16:25 +0100

    Updates

---
+++ precache-manifest.d03e1de38ad42d1a86338d8594e3cd95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8666d8ef5d2591e6a8179befcd04d7",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "3649248a3a2014ab5b4b",
+    "url": "/covid19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "a09889edac2e1cfeadf3",
+    "url": "/covid19-tracker/static/css/main.df75cfa6.chunk.css"
+  },
+  {
+    "revision": "3649248a3a2014ab5b4b",
+    "url": "/covid19-tracker/static/js/2.2a37eb2b.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid19-tracker/static/js/2.2a37eb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09889edac2e1cfeadf3",
+    "url": "/covid19-tracker/static/js/main.6b602eef.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
